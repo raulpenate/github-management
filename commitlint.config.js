aa37b80858edf09468ee5c5fdfe483bc24a0854b
raulpenate <50472064+raulpenate@users.noreply.github.com>
2025-10-12 23:13:16 -0600

    feat: add commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.export = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'types-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+  },
+};
